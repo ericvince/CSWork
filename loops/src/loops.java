63cd61010a2b2a6c23c0a1d6adc67c77de89dbcb
ericvince <vinceej1994@aguafria.org>
2018-08-21 09:00:46 -0700

    made changes in class

---
+++ loops/src/loops.java
@@ -0,0 +1,71 @@
+public class loops {
+    public static void main(String[] args) {
+        writeSquares();
+
+    }
+
+    public static void writeSquares() {
+        System.out.println("writeSquares...");
+        // We want to create a program to print the
+        // Squares of the first five natural numbers
+        // One way to do it is like this
+        System.out.println(1 + " squared = " + (1 * 1));
+        System.out.println(2 + " squared = " + (2 * 2));
+        System.out.println(3 + " squared = " + (3 * 3));
+        System.out.println(4 + " squared = " + (4 * 4));
+        System.out.println(5 + " squared = " + (5 * 5));
+        // This can be done easier with loops
+        writeSquares2();
+    }
+
+    public static void writeSquares2() {
+        System.out.println("writeSquares2...");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + " squared = " + (i * i));
+        }
+        // There are 3 parts to a for loop, they are all optional
+        // the first part is used to initialize a variable
+        // for the above for loop, we create a variable
+        // called i and initialize it to 1
+        // The middle part is the condition which will
+        // continue the loop, for the above one, the loop
+        // will continue as long as i is less than or equal
+        // to 5. The last part of the loop is used to increment
+        // or decrement the variable
+        // For the next loop, we will continue as long
+        // as i is less than or equal to 100
+        writeSquares3();
+    }
+
+    public static void writeSquares3() {
+        System.out.println("writeSquare3...");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i + " squared = " + (i * i));
+        }
+        // Lets make the next on a little more interesting
+        // we will pass the value where to stop at
+        writeSquares4(1000);
+    }
+
+    public static void writeSquares4(int stopVal) {
+        System.out.println("writeSquares4...");
+        System.out.println("stop value is " + stopVal);
+        for (int i = 1; i <= stopVal; i++) {
+            System.out.println(i + " squared = " + (i * i));
+        }
+        writeSquares5();
+    }
+
+    public static void writeSquares5()
+    {
+        System.out.println("writeSquares5...");
+        for (int c = (2+2); c <= (17 * 3); c++)
+        {
+            System.out.println(c + " squared = " + (c * c));
+        }
+        // it goes from 4 to 51 for a total
+        // of 47 loops
+        // You also don't need to count by 1s
+        //writeSquares6();
+    }
+}
